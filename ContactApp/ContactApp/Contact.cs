6c9450a44b70ebc880a15b9923cc312ad66e63b6
Unknown <mattliveuk@gmail.com>
2017-06-11 13:07:39 +0200

    Main functions added , working add new contact and namesort function

---
+++ ContactApp/ContactApp/Contact.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace ContactApp
+{
+    class Contact
+    {
+        private string firstName;
+        private string lastName;
+        private string phone;
+
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                if (value.Length == 10) {
+                    phone = value;
+                }
+                else
+                {
+                    phone = "0000000000";
+                }
+            }
+        }
+        public Contact() {
+            FirstName = "Dani";
+            LastName = "Zahoranszky";
+            Phone = "0000000000";
+            }
+
+        public Contact(string firstName, string lastName, string phone)
+        {
+            FirstName = firstName;
+            LastName = LastName;
+            Phone = phone;
+        }
+
+        public override string ToString()
+        {
+            string output = string.Empty;
+            output += String.Format("{0}, {1}", LastName, FirstName);
+            output += String.Format("({0}) {1}-{2}", Phone.Substring(0, 3), Phone.Substring(3, 3), Phone.Substring(6, 4));
+
+            return output;
+        }
+
+    }//end of class
+}// end of namespace
---
+++ ContactApp/ContactApp/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactApp
+{
+    public partial class Form1 : Form
+    {
+        //array
+        private Contact[] nameBook = new Contact[1];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Write(Contact obj)
+        {
+            StreamWriter sw = new StreamWriter("Contacts.txt");
+            sw.WriteLine(nameBook.Length + 1);
+            sw.WriteLine(obj.FirstName);
+            sw.WriteLine(obj.LastName);
+            sw.WriteLine(obj.Phone);
+
+            for(int i = 0; i < nameBook.Length; i++)
+            {
+                sw.WriteLine(nameBook[i].FirstName);
+                sw.WriteLine(nameBook[i].LastName);
+                sw.WriteLine(nameBook[i].Phone);
+            }
+
+            sw.Close();
+        }
+
+        private void Read()
+        {
+            StreamReader sr = new StreamReader("Contacts.txt");
+            nameBook = new Contact[Convert.ToInt32(sr.ReadLine())];
+
+            for ( int i = 0; i < nameBook.Length; i++)
+            {
+                nameBook[i] = new Contact();
+                nameBook[i].FirstName = sr.ReadLine();
+                nameBook[i].LastName = sr.ReadLine();
+                nameBook[i].Phone = sr.ReadLine();
+                
+            }
+
+            sr.Close();
+
+        }
+
+        private void Display()
+        {
+            lstContacts.Items.Clear();
+
+            for( int i = 0; i < nameBook.Length; i++)
+            {
+                lstContacts.Items.Add(nameBook[i].ToString());
+            }
+        }
+
+        private void ClearList()
+        {
+            txtFirstName.Text = String.Empty;
+            txtLastName.Text = String.Empty;
+            txtPhone.Text = String.Empty;
+        }
+
+        private void btnAddContact_Click(object sender, EventArgs e)
+        {
+            Contact obj = new Contact();
+            obj.FirstName = txtFirstName.Text;
+            obj.LastName = txtLastName.Text;
+            obj.Phone = txtPhone.Text;
+
+            Write(obj);
+            NameSort();
+            Read();
+            Display();
+            ClearList();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Read();
+            Display();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NameSort();
+            Display();
+        }
+
+        private void NameSort()
+        {
+            Contact temp;
+            bool swap;
+
+            do
+            {
+                swap = false;
+
+                for (int i = 0; i < (nameBook.Length - 1); i++)
+                {
+                    if(nameBook[i].LastName.CompareTo(nameBook[i+1].LastName) > 0)
+                    {
+                        temp = nameBook[i];
+                        nameBook[i] = nameBook[i + 1];
+                        nameBook[i + 1] = temp;
+                        swap = true;
+                    }
+                }
+
+
+            } while (swap == true);
+        }
+    }
+}
